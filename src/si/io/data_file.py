f8dce4784f1a2c6c1b88c3ce5d75831325215050
mleiras <monicarafaelaleiras@gmail.com>
2022-09-28 22:47:15 +0100

    Update of module data_file & csv

--- src/si/io/data_file.py
+++ src/si/io/data_file.py
@@ -1,5 +1,37 @@
-def read_data_file():
-    '''
-    Podemos utilizar o np.genfromtxt separado por espaços ou qualquer coisa, fazer o mesmo que o read_csv mas aqui le um ficheiro txt e tem de retornar o numpy array
-    '''
+from re import A
+import sys
+from tkinter.filedialog import asksaveasfile
+sys.path.insert(0, '/home/monica/Documents/2_ano/sistemas/si/src/si')
+import numpy as np
+from data.dataset import Dataset
+
+
+def read_data_file(filename: str, sep: str, label: bool or list):
+    data = np.genfromtxt(filename, delimiter=sep, names=label)
+    if label is True:
+        pass
+    y = None
+    features = None
+    return Dataset(data, y, features, label)
+
+
+def write_data_file():
     pass
+
+
+if __name__ == '__main__':
+    teste = read_data_file('si/datasets/breast-bin.data', sep=',', label=None)
+    # teste = read_data_file('si/datasets/iris.csv', sep=',', label=True)
+    print(teste.shape())
+    print(teste.y)
+    print(teste.X)
+    print('---------------------------------------------------------------')
+    print('Shape: \n', teste.shape())
+    print('Label: \n', teste.has_label())
+    print('Classes: \n', teste.get_classes())
+    print('Média: \n', teste.get_mean())
+    print('Variância: \n', teste.get_variance())
+    print('Mediana: \n', teste.get_median())
+    print('Min: \n', teste.get_min())
+    print('Max: \n', teste.get_max())
+    print('Summary: \n', teste.summary())
--- src/si/io/module_csv.py
+++ src/si/io/module_csv.py
@@ -1,16 +1,12 @@
 import sys
 sys.path.insert(0, '/home/monica/Documents/2_ano/sistemas/si/src/si')
-
-# print(sys.path)
-
-from data.dataset import Dataset
 import pandas as pd
+from data.dataset import Dataset
+
 
 
 def read_csv(filename: str, sep: str, features: bool, label: bool):
-    # from data.dataset import Dataset
-    # utilizar pd.read_csv para retornar o df
-    # Tem de reetornar Dataset(df[] que é o x, df[] que é o y, features, label)
+
     data = pd.read_csv(filename, sep)
     if features and label:  # se tiver nomes nas colunas e a variavel y
         print('TEM FEATURES E Y')
@@ -18,7 +14,7 @@ def read_csv(filename: str, sep: str, features: bool, label: bool):
         label = data.columns[-1]
         data = data.iloc[:, :-1]
         features = data.columns
-        dataset = Dataset(data, y=y, features=features)
+        dataset = Dataset(data, y=y, features=features, label=label)
 
     elif features and label is False:  # se tiver nomes nas colunas mas não tem y
         print('TEM FEATURES')
@@ -30,7 +26,7 @@ def read_csv(filename: str, sep: str, features: bool, label: bool):
         y = data.iloc[:, -1]
         label = data.columns[-1]
         data = data.iloc[:, :-1]
-        dataset = Dataset(data, y=y)
+        dataset = Dataset(data, y=y, label=label)
 
     else:  # quando não tem nem nomes nas colunas nem tem y nos dados
         print('NÃO TEM NADA')
@@ -47,11 +43,13 @@ if __name__ == '__main__':
     teste = read_csv('si/datasets/iris.csv', sep=',',
                      features=True, label=True)
     print(teste.X.head(10))
-
-
-'''
-    x = np.array([[1, 2, 3], [3, 1, 3]])
-    y = np.array([1, 2])
-    features = ['A', 'B', 'C']
-    dataset = Dataset(x, y, features, 'as')
-'''
+    print('---------------------------------------------------------------')
+    print('Shape: \n', teste.shape())
+    print('Label: \n', teste.has_label())
+    print('Classes: \n', teste.get_classes())
+    print('Média: \n', teste.get_mean())
+    print('Variância: \n', teste.get_variance())
+    print('Mediana: \n', teste.get_median())
+    print('Min: \n', teste.get_min())
+    print('Max: \n', teste.get_max())
+    print('Summary: \n', teste.summary())
